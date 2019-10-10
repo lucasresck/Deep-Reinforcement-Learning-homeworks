19bc8c7889fc5a19e229c0d29b160d235f857de3
Lucas Emanuel Resck Domingues <41991486+lucasresck@users.noreply.github.com>
2019-10-09 21:42:46 -0300

    Update run_agent.bash

--- hw1/run_agent.bash
+++ hw1/run_agent.bash
@@ -2,5 +2,5 @@
 set -eux
 for e in Hopper-v2 Ant-v2 HalfCheetah-v2 Humanoid-v2 Reacher-v2 Walker2d-v2
 do
-    python run_agent.py $e --render --num_rollouts 5
+    python run_agent.py dagger $e --render --num_rollouts 5
 done
